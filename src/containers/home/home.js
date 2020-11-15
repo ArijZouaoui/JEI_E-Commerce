9dd526e708a57e0d8b2b39565595f8da13d778d7
Ayari17 <ayari17@hotmail.fr>
2020-11-15 22:33:45 +0100

    initial commit

---
+++ src/containers/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return(
+        <>
+            <h4>
+                Home page
+            </h4>
+
+
+        </>
+    );
+
+}
+export default Home;
