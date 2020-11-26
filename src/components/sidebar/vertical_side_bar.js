06a3e7c6221270e978a18513251d05866341b3e6
Sarah Benabdallah <71703426+SarahBenabdallah@users.noreply.github.com>
2020-11-26 20:09:59 +0100

    Add categories

--- src/components/sidebar/vertical_side_bar.js
+++ src/components/sidebar/vertical_side_bar.js
@@ -1,42 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 import './vertical_side_bar.css'
 
 
-const VerticalSideBar = () => {  
-return(
-<Card style={{marginTop: 20, marginBottom: 20, width: '13rem', display:'inline-grid'}} className='card text-center'>
-<div className="ui vertical text-center" >
-  <div className="item">
-    <div className="header">SOFAS</div>
-    <div className="menu">
-    <a  className="item">trendy</a>
-    <a  className="item">expensive</a>
-    <a  className="item">elegant</a>
-    </div>
-  </div>
-  <div className="item">
-    <div className="header">COUCHES</div>
-    <div className="menu">
-    <a  className="item">trendy</a>
-    <a  className="item">expensive</a>
-    <a  className="item">elegant</a>
-    </div>
-  </div>
-  <div className="item">
-    <div className="header">TABLES</div>
-    <div className="menu">
-    <a  className="item">trendy</a>
-    <a  className="item">expensive</a>
-    <a  className="item">elegant</a>
-    </div>
-  </div>
-</div>
-</Card>
+const VerticalSideBar = (props) => {
+  const [textColor, setTextColor] = useState({ sofa: "#777", tables: "#777", couche: "#777" })
+  const appStylesSofa = {
+    color: `${textColor['sofa']}`
+  }
+  const appStylesTables = {
+    color: `${textColor['tables']}`
+  }
+  const appStylesCouche = {
+    color: `${textColor['couche']}`
+  }
+
+
+  return (
+    <Card style={{ marginTop: 20, marginBottom: 20, width: '13rem', display: 'inline-grid' }} className='card text-center'>
+      <div className="ui vertical text-center" >
+        <div className="item">
+          <div className="header" onClick={() => props.handleChange('', 'sofa')} style={{ ...appStylesSofa, cursor: 'pointer' }} onMouseEnter={() => setTextColor({sofa: "#003fcc"})} onMouseLeave={() => setTextColor({sofa: "#777"})}> SOFAS </div>
+          <div className="menu">
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('trendy', 'sofa')}>trendy</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('expensive', 'sofa')}>expensive</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('elegant', 'sofa')}>elegant</a>
+          </div>
+        </div>
+        <div className="item">
+          <div className="header" onClick={() => props.handleChange('', 'couche')} style={{ ...appStylesCouche, cursor: 'pointer' }} onMouseEnter={() => setTextColor({couche:"#003fcc"})} onMouseLeave={() => setTextColor({couche:"#777"})}>COUCHES</div>
+          <div className="menu">
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('trendy', 'couche')}>trendy</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('expensive', 'couche')}>expensive</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('elegant', 'couche')}>elegant</a>
+          </div>
+        </div>
+        <div className="item">
+          <div className="header" onClick={() => props.handleChange('', 'table')} style={{ ...appStylesTables, cursor: 'pointer' }} onMouseEnter={() => setTextColor({tables:"#003fcc"})} onMouseLeave={() => setTextColor({tables:"#777"})}>TABLES</div>
+          <div className="menu">
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('trendy', 'table')}>trendy</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('expensive', 'table')}>expensive</a>
+            <a className="item" style={{ cursor: 'pointer' }} onClick={() => props.handleChange('elegant', 'table')}>elegant</a>
+          </div>
+        </div>
+      </div>
+    </Card>
 
 
 
-)
+  )
 }
 
-export default VerticalSideBar ;
+export default VerticalSideBar;
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import Dropdown from '../../components/sidebar/dropdown';
 import VerticalSideBar from '../../components/sidebar/vertical_side_bar'
 import ProductList from '../../components/card/product_list'
@@ -6,17 +6,30 @@ import {products} from '../../data/products'
 import {Container , Row, Col} from 'react-bootstrap'
 
 const Home = () => {
+  
+
+    const [categoryProduct ,setCategoryProduct] = useState({
+        category : '',
+        name : ''
+    });
+
+
+    const handleChange = (category,name) => {
+        setCategoryProduct({category,name});
+      } 
+
+    
     return(
         <div className="row">
         <Container>
         <Row>
         <Col xs={2} md={4}>
-        <VerticalSideBar /> 
+        <VerticalSideBar handleChange={handleChange} /> 
         <br />
         <Dropdown />
         </Col>
         <Col xs={4} md={8}>
-        <ProductList products={products} />
+        <ProductList products={products} categoryProduct={categoryProduct} handleChange={handleChange} />
         </Col>
         </Row>
         </Container>
