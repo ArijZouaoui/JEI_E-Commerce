189d6660f8d040e10c35df55492f24eac8007efb
Arij Zouaoui <arijzouaoui@gmail.com>
2020-12-20 23:17:16 +0100

    update

--- src/components/addProduct/addProduct.js
+++ src/components/addProduct/addProduct.js
@@ -2,37 +2,45 @@ import React, { Component } from 'react';
 import {Form, Modal, Button, Row, Col, } from 'react-bootstrap';
 import img from '../../assets/defaultpic.png';
 import './addProduct.css'
+import axios from 'axios';
 
 class AddProduct extends Component {
-    state = {
-    }
-
+    state = {}
     constructor(props) {
       super(props)
       this.state = { 
         imagePreviewUrl:'',
-        file:'',
         error:String,
         productName:'',
+        productDsc:'',
+        productPrice:'',
+        productCategory:'',
         errorList: [], 
         showErrors: Boolean,
-        disabledButton: true}
+        disabledButton: true,
+        file: null, 
+
+    };
+
       this.handleName = this.handleName.bind(this);
+      this.handleDsc = this.handleDsc.bind(this);
+      this.handlePrice = this.handlePrice.bind(this);
+      this.handleCategory = this.handleCategory.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this._handleImageChange = this._handleImageChange.bind(this);
      
     }
 
-    _handleImageChange(e) {
+      _handleImageChange(e) {
         e.preventDefault();
     
         let reader = new FileReader();
         let file = e.target.files[0];
-    
         reader.onloadend = () => {
           this.setState({
             file: file,
-            imagePreviewUrl: reader.result
+            imagePreviewUrl: reader.result,
+            
           });
         }
     
@@ -45,62 +53,171 @@ class AddProduct extends Component {
             nameError = 'Name is required';
         }
         return nameError;
-    };
+      };
       productPriceValidator(price) {
         var priceError = '';
         if (!price || price <= 0) {
             priceError = 'Price is required';
         }
         return priceError;
-    };
+      };
       productDscValidator(dsc) {
         var dscError = '';
         if (!dsc || dsc.length === 0) {
             dscError = 'Description is required';
         }
         return dscError;
-    };
+      };
       productCategoryValidator(category) {
-        var categoryError = '';
-        if (!category || category.length === 0) {
-            categoryError = 'Category is required';
+          var categoryError = '';
+          if (!category || category.length === 0) {
+              categoryError = 'Category is required';
+          }
+          return categoryError;
+      };
+      handleName (event) {
+        var name = "" + event.target.value;
+        var error = this.state.error;
+        error.nameMessage = this.productNameValidator(name);
+        if ((this.state.errorList.indexOf(error.nameMessage) <= -1) && error.nameMessage!='') {
+          this.setState({errorList:this.state.errorList.concat(error.nameMessage)})
         }
-        return categoryError;
+        var index = this.state.errorList.indexOf('Name is required');
+        console.log(index)
+        console.log(error.nameMessage)
+        if (index==0){
+          this.setState({errorList:this.state.errorList.splice(index,1)});
+        }
+        if ((index>-1)&& error.nameMessage==''){
+          this.setState({errorList:this.state.errorList.splice(index-1,1)});
+
+        }
+        if (error.nameMessage ==''){
+        this.setState({
+            productName: name,
+            error: error
+        });
+      }};
+      handleDsc (event) {
+          var dsc = "" + event.target.value;
+          var error = this.state.error;
+          
+          error.dscMessage = this.productDscValidator(dsc);
+          if ((this.state.errorList.indexOf(error.dscMessage) <= -1) && error.dscMessage!='') {
+            this.setState({errorList:this.state.errorList.concat(error.dscMessage)})
+          }
+          var index = this.state.errorList.indexOf('Description is required');
+          console.log(index)
+          console.log(error.dscMessage)
+          if (index==0){
+            this.setState({errorList:this.state.errorList.splice(index,1)});
+          }
+          if ((index>-1)&& error.dscMessage==''){
+            this.setState({errorList:this.state.errorList.splice(index-1,1)});
+
+          }
+          if (error.dscMessage==""){
+          this.setState({
+              productDsc: dsc,
+              error: error
+          });
+        }
+      };
+      handlePrice (event) {
+        var price = "" + event.target.value;
+        var error = this.state.error;
+        
+        error.priceMessage = this.productPriceValidator(price);
+        if ((this.state.errorList.indexOf(error.priceMessage) <= -1) && error.priceMessage!='') {
+          this.setState({errorList:this.state.errorList.concat(error.priceMessage)})
+        }
+        var index = this.state.errorList.indexOf('Price is required');
+        console.log(index)
+        console.log(error.priceMessage)
+        if (index==0){
+          this.setState({errorList:this.state.errorList.splice(index,1)});
+        }
+        if ((index>-1)&& error.priceMessage==''){
+          this.setState({errorList:this.state.errorList.splice(index-1,1)});
+
+        }
+        if (error.priceMessage==''){
+        this.setState({
+            productPrice: price,
+            error: error
+        });
+      }
+      };
+      handleCategory (event) {
+        var category = "" + event.target.value;
+        var error = this.state.error;
+        
+        error.categoryMessage = this.productCategoryValidator(category);
+        if ((this.state.errorList.indexOf(error.categoryMessage) <= -1) && error.categoryMessage!='') {
+          this.setState({errorList:this.state.errorList.concat(error.categoryMessage)})
+        }
+        var index = this.state.errorList.indexOf('Category is required');
+        console.log(index)
+        console.log(error.categoryMessage)
+        if (index==0){
+          this.setState({errorList:this.state.errorList.splice(index,1)});
+        }
+        if ((index>-1)&& error.categoryMessage==''){
+          this.setState({errorList:this.state.errorList.splice(index-1,1)});
+
+        }
+        if (error.categoryMessage==''){
+          
+        this.setState({
+            productCategory: category, 
+            error: error
+        });
+      }
+
+      };
+    handleSubmit(e){
+      e.preventDefault();
+    const formData = new FormData();
+    formData.append('file',this.state.file);
+    formData.append('productName', this.state.productName);
+    formData.append('productCategory', this.state.productCategory);
+    formData.append('productPrice', this.state.productPrice);
+    formData.append('productDsc', this.state.productDsc);
+
+    for(var pair of formData.entries()) {
+      console.log(pair[0]+ ', '+ pair[1]); 
+   }
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        },
+        processData: false,
     };
-    handleName (event) {
-      var name = "" + event.target.value;
-      var error = this.state.error;
+    axios.post("http://localhost:5000/api/products/post",formData,config)
+        .then((response) => {
+            alert("The file is successfully uploaded");
+        }).catch((error) => {
+          console.log(error)
+    });
 
-      error.nameMessage = this.productNameValidator(name);
-
-      this.setState({
-          error: error,
-          productName: name
-      });
-  };
-    handleSubmit(){
-     var nameError = this.state.error.nameMessage;
-    console.log(nameError)
-    var errors = this.state.errorList;
-    if (errors){
-      this.setState({showErrors : true});
-    }
             
     }
     render() {
         let {imagePreviewUrl, error,disabledButton, errorList} = this.state;
-        console.log(error.open)
         let $imagePreview = null;
         var errorText = null;
         if (imagePreviewUrl) {
           $imagePreview = (<img src={imagePreviewUrl} />);
         }
         if (error) {
-          errorText = error.nameMessage ;//+ " " + error.passwordMessage + " " + error.emailMessage;
-      } 
+          errorText = errorList.toString()
+          console.log(errorText)
+          console.log(errorList)
+        } 
       if (!errorText)
       {
           disabledButton = false;
+        
           errorText = error.message;
       }
        
@@ -122,11 +239,13 @@ class AddProduct extends Component {
             <Modal.Body>
              
                  <Row>
-                     <p>{error.nameMessage}</p>
+                     
                          <Form onSubmit={this.handleSubmit}
                          onChange={this.onChangeHandler}
+                        className="row"
                          >
-                             <Form.Group controlId="name">
+                             <Form.Group className="col-md-6" className="col-md-6" controlId="name">
+                             <p>{error.nameMessage}</p>
                              <Form.Label>Product name</Form.Label>
                                  <Form.Control
                                  type='text'
@@ -134,36 +253,40 @@ class AddProduct extends Component {
                                  onChange={this.handleName}
                                  />
                              </Form.Group>
-                             <Form.Group controlId="price">
+                             <Form.Group className="col-md-6" controlId="price">
+                             <p>{error.priceMessage}</p>
                                  <Form.Label>Price</Form.Label>
                                  <Form.Control
                                  type='number'
                                  name='price'
-                                 
+                                 onChange={this.handlePrice}
                                  placeholder=''
                                  
                                  />
-                             <Form.Group controlId="description">
+                             </Form.Group>
+                             <Form.Group className="col-md-6" controlId="description">
+                             <p>{error.dscMessage}</p>
                                  <Form.Label>Description</Form.Label>
                                  <Form.Control
                                  type='text'
                                  name='description'
-                                 
+                                 onChange={this.handleDsc}
                                  placeholder=''
                                  
                                  />
                              </Form.Group>
-                             <Form.Group controlId="category">
+                             <Form.Group className="col-md-6" controlId="category">
+                             <p>{error.categoryMessage}</p>
                                  <Form.Label>Category</Form.Label>
                                  <Form.Control
                                  type='text'
                                  name='category'
-                                 
+                                 onChange={this.handleCategory}
                                  placeholder=''
                                  
                                  />
                              </Form.Group>
-                             
+                           <Form.Group className="col-md-6">  
                          <Form.File id="exampleFormControlFile1" 
                          label="Product image"
                          onChange={this._handleImageChange} />
@@ -173,10 +296,10 @@ class AddProduct extends Component {
                                 width='200'
                                 />
                          </Form.Group>
-                         <Form.Group>
+                         <Form.Group >
                              <Button vaiant='primary'
                              type='submit'
-                             disabled ={this.disabledButton}
+                             disabled ={disabledButton}
                              onClick={this.handleSubmit}>
                                  Add 
                              </Button>
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -16,7 +16,9 @@ class Dashboard extends Component {
       .then(response=>{
       this.setState({commandes: response.data});
       console.log(response)
-      })
+      console.log(response.data)
+
+    })
       .catch(function(error){console.log(error)});
         
          
@@ -37,7 +39,7 @@ class Dashboard extends Component {
         return (
             <React.Fragment>
                 <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">Navbar</span>
+                <span class="navbar-brand mb-0 h1">Add Product</span>
                 <button type="button" class="btn btn-outline-light" 
                 onClick={this.handle}>
                     <img src={addIcon}></img>
@@ -47,12 +49,17 @@ class Dashboard extends Component {
         <table class="table table-hover">
         <thead>
           <tr>
-            <th scope="col">id</th>
-            <th scope="col">Product id</th>
-            <th scope="col">Name</th>
+            <th scope="col">Order ID</th>
+            <th scope="col">User ID</th>
+            <th scope="col">Username</th>
             <th scope="col">Phone Number</th>
+            <th scope="col">Address</th>
+            <th scope="col">Product ID</th>
+            <th scope="col">Product Name</th>
             <th scope="col">Quantity</th>
-            <th scope="col">Command Date</th>
+            <th scope="col">Order Date</th>
+            <th scope="col">Delivery Date</th>
+
           </tr>
         </thead>
         <tbody>
@@ -62,12 +69,16 @@ class Dashboard extends Component {
                         i++
                         return(
                             <tr>
-                            <th scope="row">{i}</th>
-                            <td>{current.id}</td>
-                            <td>{current.orderDate}</td>
-                            <td>{current.deliveryDate}</td>
-                            <td>{current.cartId}</td>
-                            <td>{current.createdAt}</td>
+                            <td>{current['id']}</td>
+                            <td>{current['Users.id']}</td>
+                            <td>{current['Users.username']}</td>
+                            <td>{current['Users.phoneNumber']}</td>
+                            <td>{current['Users.address']}</td>
+                            <td>{current['Products.id']}</td>
+                            <td>{current['Products.name']}</td>
+                            <td>{current['Products.quantity']}</td>
+                            <td>{current['orderDate']}</td>
+                            <td>{current['deliveryDate']}</td>
                             </tr>
                         )})
                     } 
