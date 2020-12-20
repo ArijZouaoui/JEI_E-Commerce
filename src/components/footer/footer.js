e199edfb5f9e717aec4c583a74645cfe15532902
Arij Zouaoui <arijzouaoui@gmail.com>
2020-12-20 23:58:14 +0100

    footer-update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,38 +1,28 @@
 import React from "react";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedinIcon from "@material-ui/icons/LinkedIn";
+import { Link } from "react-router-dom";
 import "./footer.css";
 function Footer() {
   return (
-    <footer className="footer-container">
-      <ul className="footer-list">
-        <li className="footer-list-title">INFORMATION</li>
-        <li className="footer-list-option">About Us</li>
-        <li className="footer-list-option">Delivery Information</li>
-        <li className="footer-list-option">Privacy Policy</li>
-        <li className="footer-list-option">Terms & Conditions</li>
-      </ul>
-      <ul className="footer-list">
-        <li className="footer-list-title">CUSTOMER SERVICE</li>
-        <li className="footer-list-option">Contact Us</li>
-        <li className="footer-list-option">Returns</li>
-        <li className="footer-list-option">Site Map</li>
-        <li className="footer-list-option">Shipping</li>
-      </ul>
+    <footer className="footer-container" style={{height:"20vh"}}>
 
-      <ul className="footer-list">
-        <li className="footer-list-option1">Copyright © nicole_89</li>
+      <ul className="footer-list" style={{alignItems:'center'}}>
+        <li className="footer-list-option1" style={{marginTop:'10px'}}>Copyright © JEI_TEAM_4</li>
         <div className="footer-media">
-          <li className="footer-list-option">
-            <FacebookIcon />
+          <li className="footer-list-option"><a href='https://www.facebook.com/junior.entreprise.insat'>
+            <FacebookIcon /></a>
+
           </li>
 
           <li className="footer-list-option">
-            <InstagramIcon />
+            <a href='https://www.instagram.com/junior_entreprise_insat/?hl=fr'>
+            <InstagramIcon /></a>
           </li>
           <li className="footer-list-option">
-            <TwitterIcon />
+            <a href='https://www.linkedin.com/company/junior-entreprise-insat/'>
+            <LinkedinIcon /></a>
           </li>
         </div>
       </ul>
